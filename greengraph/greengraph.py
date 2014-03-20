dbab10ae627411ad4595e3c8002432b27fa02e82
James Hetherington <j.hetherington@ucl.ac.uk>
2014-03-20 14:50:17 +0000

    Generate URL for talking to google maps

--- greengraph/greengraph.py
+++ greengraph/greengraph.py
@@ -7,3 +7,21 @@ def geolocate(place):
   Given a string description of a place, return it's latitude and longitude.
   """
   return geocoder.geocode(place)[1]
+
+def maps_url_for(lat,long,
+  satellite=False,zoom=12,size=(400,400),sensor=False):
+  url="http://maps.googleapis.com/maps/api/staticmap?"
+  params=dict(
+    sensor= str(sensor).lower(),
+    zoom= 12,
+    size= "x".join(map(str,size)),
+    center= ",".join(map(str,(lat,long))),
+  )
+
+  if satellite:
+    params["maptype"]="satellite"
+
+  paramstring="&".join([key+"="+str(value) for
+    key,value in params.items()])
+
+  return url+paramstring
--- greengraph/test/test_greengraph.py
+++ greengraph/test/test_greengraph.py
@@ -1,7 +1,19 @@
-from nose.tools import assert_almost_equal
-from ..greengraph import geolocate
+from nose.tools import assert_almost_equal, assert_in
+from ..greengraph import geolocate, maps_url_for
 
 def test_geolocate():
   latlong=geolocate("London")
   assert_almost_equal(latlong[0],51.5072,places=2)
   assert_almost_equal(latlong[1], -0.1275,places=2)
+
+def test_generate_url():
+  url=maps_url_for(51.5072,-0.1275)
+  assert_in("http://maps.googleapis.com/maps/api/staticmap?",url)
+  assert_in("center=51.5072,-0.1275",url)
+  assert_in("zoom=12",url)
+  assert_in("size=400x400",url)
+  assert_in("sensor=false",url)
+
+def test_generate_satellite_url():
+  url=maps_url_for(51.5072,-0.1275,satellite=True)
+  assert_in("maptype=satellite",url)
