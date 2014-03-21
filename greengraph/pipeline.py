ce9b6ea0a1c6b7f67fd6e4b382d43b09c14c15cc
James Hetherington <j.hetherington@ucl.ac.uk>
2014-03-21 03:39:53 +0000

    Don't use subdirs as it makes exercise more complex

--- greengraph/pipeline.py
+++ greengraph/pipeline.py
@@ -9,26 +9,26 @@ locations=open('locations').readlines()
 # Make an image file for each location
 for location in locations:
   data=get_png_at(*geolocate(location))
-  buffer=open(join('images',location[:-1])+'.png','w')
+  buffer=open(location[:-1]+'.png','w')
   buffer.write(data.read())
 
 # For each image file, turn it into a csv file of pixel triples
-for image in glob('images/*.png'):
+for image in glob('*.png'):
   data=open(image)
   rows=read_png(data)
   pix=pixels(rows)
-  location=splitext(basename(image))[0]
-  buffer=open(join('pixelfiles',location)+'.csv','w')
+  location=splitext(image)[0]
+  buffer=open(location+'.csv','w')
   writer=csv.writer(buffer)
   for pixel in pix:
     writer.writerow(pixel)
 
 # For each csv file, count the green pixels
-buffer=open('results.csv','w')
+buffer=open('results','w')
 writer=csv.writer(buffer)
-for pixelfile in glob('pixelfiles/*.csv'):
+for pixelfile in glob('*.csv'):
   data=open(pixelfile)
   reader=csv.reader(data)
-  location=splitext(basename(pixelfile))[0]
+  location=splitext(pixelfile)[0]
   count=count_green_pixels(row for row in reader)
   writer.writerow([location,count])
