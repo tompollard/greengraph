87741367cd87ff5f0323293850fc2b32b928635d
Tom Pollard <tom.pollard.11@ucl.ac.uk>
2015-03-01 17:25:27 +0000

    autopep8 tests

--- greengraph/test/test_greengraph.py
+++ greengraph/test/test_greengraph.py
@@ -6,46 +6,54 @@ from ..greengraph import greengraph
 import png
 import os
 
+
 def example():
-  return png.Reader(filename=(os.path.join(
-  os.path.dirname(__file__),"fixtures","london.png"))).asRGB()
+    return png.Reader(filename=(os.path.join(
+        os.path.dirname(__file__), "fixtures", "london.png"))).asRGB()
+
 
 def test_geolocate():
-  latlong=geolocate("London")
-  assert_almost_equal(latlong[0],51.5072,places=2)
-  assert_almost_equal(latlong[1], -0.1275,places=2)
+    latlong = geolocate("London")
+    assert_almost_equal(latlong[0], 51.5072, places=2)
+    assert_almost_equal(latlong[1], -0.1275, places=2)
+
 
 def test_generate_url():
-  url=maps_url_for(51.5072,-0.1275)
-  assert_in("http://maps.googleapis.com/maps/api/staticmap?",url)
-  assert_in("center=51.5072,-0.1275",url)
-  assert_in("zoom=12",url)
-  assert_in("size=400x400",url)
-  assert_in("sensor=false",url)
+    url = maps_url_for(51.5072, -0.1275)
+    assert_in("http://maps.googleapis.com/maps/api/staticmap?", url)
+    assert_in("center=51.5072,-0.1275", url)
+    assert_in("zoom=12", url)
+    assert_in("size=400x400", url)
+    assert_in("sensor=false", url)
+
 
 def test_generate_satellite_url():
-  url=maps_url_for(51.5072,-0.1275,satellite=True)
-  assert_in("maptype=satellite",url)
+    url = maps_url_for(51.5072, -0.1275, satellite=True)
+    assert_in("maptype=satellite", url)
+
 
 def test_get_png():
-  london=(51.508515, -0.1254872)
+    london = (51.508515, -0.1254872)
 
-  map=get_map_at(*london)
-  # Can't compare against fixture because online map changes
-  assert_equal(map[3]['size'],(400,400))
+    map = get_map_at(*london)
+    # Can't compare against fixture because online map changes
+    assert_equal(map[3]['size'], (400, 400))
+
 
 def test_count_green():
-  assert_equal(count_green(example()),15957)
+    assert_equal(count_green(example()), 15957)
+
 
 def test_isgreen():
-  assert(is_green(10,50,5))
-  assert_false(is_green(50,10,5))
+    assert(is_green(10, 50, 5))
+    assert_false(is_green(50, 10, 5))
+
 
 def test_locationsequence():
-  result=location_sequence((0,0),(10,10),5)
-  assert_equal(result[0],(0,0))
-  assert_equal(result[1],(2.5,2.5))
+    result = location_sequence((0, 0), (10, 10), 5)
+    assert_equal(result[0], (0, 0))
+    assert_equal(result[1], (2.5, 2.5))
 
-#def test_driver():
+# def test_driver():
 #  result=greengraph("London","Cambridge, England",10)
 #  assert_equal(len(result),10)
